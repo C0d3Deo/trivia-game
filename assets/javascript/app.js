7ef3f4c28363d5c6aba58e2cc1e8fd475c0297b3
de0lan10 <deonte.lang@gmail.com>
2016-10-26 07:38:10 -0700

    alpha

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,98 +1,70 @@
-var wins = 0;
-var losses = 0;
-var goal = "";
+var counter = 30.00;
+var interval;
 var score = 0;
-var crystals = [];
+var right = 0;
+var wrong = 0;
+
+function count (){
+	counter -= 0.01;
+}
+
+var countDwn = setInterval(count, 10);
+	
+function start(){
+    // I've realized that my timer doesn't count in actual tenths of seconds due to an inherent problem of the javascript language. The solutions to fix it seemed like more trouble than it was worth. Users have "about" 16 seconds which is fine to me.
+    countDwn;
+    $("#counterBox").html(counter.toFixed(2));
+} 
 
 $(document).ready(function() {
+	// $("input[type=radio]").prop("checked", false);
+	interval = setInterval(start,10)
 
-//This code was copied from the internet however I will comment my understanding of the function
-$('<div id="overlay">').css({//creating overlay div
-      "width" : "100%"
-    , "height" : "100%" //both lines are to cover entire window
-    , "background" : "#000" //set black background
-    , "position" : "fixed" //static imposition
-    , "top" : "0"
-    , "left" : "0" //rest any inherent margins from the browser
-    , "zIndex" : "50" //bring div to forefront of page
-    , "MsFilter" : "progid:DXImageTransform.Microsoft.Alpha(Opacity=60)"
-    , "filter" : "alpha(opacity=60)"
-    , "MozOpacity" : 0.6
-    , "KhtmlOpacity" : 0.6
-    , "opacity" : 0.6 //all the above are cross-browser compatability commands that will all do the same thing (create transparency) but will be picked up or ignored based on the browser
+    $("#submit").on("click", function (){
+    	clearInterval(interval);
+    	score = Math.round(counter);
+    	var numQuestions = $(".question").length;
+    	for (var i = 0; i < numQuestions; i++) {
+    		var questions = document.getElementsByClassName("question");
+    		var selectValue =  questions[i].input[questions[i].checkedIndex].val();
+    		if (selectedValue == "wrong"){
+    			score += -2;
+    			++wrong;
+    			console.log(score);
+    			console.log(wrong);
+    		}else if (selectedValue == "right"){
+    			score += 4;
+    			++right;
+    			console.log(score);
+    			console.log(right);
+    		}
+    	}
+    	// setTimeout( alert("Time Left: " + counter +"\n" +
+    	// 		"Not Wrong: " + right + "\n" +
+    	// 		"Not Not Anti-Right: " + wrong), 5000);
+    	// $("#timeScore", "#rightScore", "wrongScore").html(counter, right, wrong);
 
-}).appendTo(document.body); //add this created overlay to the body of the webpage
-$("#instructPopup").show();
-$('#close').click(function(){
-	$("#instructPopup").remove();
-	$("#overlay").remove();
-});
+  //   	$('<div id="overlay">').css({//creating overlay div
+		//       "width" : "100%"
+		//     , "height" : "100%" //both lines are to cover entire window
+		//     , "background" : "#000" //set black background
+		//     , "position" : "fixed" //static imposition
+		//     , "top" : "0"
+		//     , "left" : "0" //rest any inherent margins from the browser
+		//     , "zIndex" : "50" //bring div to forefront of page
+		//     , "MsFilter" : "progid:DXImageTransform.Microsoft.Alpha(Opacity=60)"
+		//     , "filter" : "alpha(opacity=60)"
+		//     , "MozOpacity" : 0.6
+		//     , "KhtmlOpacity" : 0.6
+		//     , "opacity" : 0.6 //all the above a
+		//     }).appendTo(document.body);
+	 //   $("#dialog-1").css({
+	 //   		"width" : "col-md-4",
+	 //   		"background" : "white",
+	 //   		"position" : "fixed",
+	 //   		"zIndex" : "50"
+	 //   })
+		// .show(); 
+    });
 
-	function game (){
-	
-		goal = 
-			Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-			$("#crysGoal").html(goal);
-		for (var i = 0; i < 4; i++) {
-			do	{var value = Math.floor(Math.random() * (12)) + 1;}
-		 	while(crystals.indexOf(value) !== -1 ) 
-			crystals.push(value);
-		}
-	} game(goal, crystals); 
-
-	function reset (){
-		 goal = "";
-		 score = 0;
-		 crystals = [];
-		$(".scoreCntr").html(0);
-	}
-
-	//This function compounds on top of itself after each game. I have no idea how to stop it. I've tried using return in various places. I've tried giving it its own function. I've tried including it in my reset (using return). I don't even know what to search to get help. Google wasn't giving me relevant issues this time around.
-	function check (){
-		if (score === goal) {
-			var audio = new Audio("../week-4-game/assets/images/oh-yeah.mp3");
-					audio.play();
-			alert("Yay!");
-			wins++;
-			$("#wins").empty().html(wins);
-			reset ();
-			game ();
-		}
-		if (score > goal) {
-			losses++;
-			$("#losses").empty().html(losses);
-			alert("YOU'VE LOST THE GAME!!! <O>____<O>");
-			reset();
-			game();
-		}
-	}
-		if (score < goal) {
-			$(".crystals").click(function userClick (){
-					if ($(this).is("#crys1")){
-						console.log(crystals[0]);
-						score += crystals[0];
-						$(".scoreCntr").html(score);
-						check();
-					}; 
-					if ($(this).is("#crys2")){
-						console.log(crystals[1]);
-						score += crystals[1];
-						$(".scoreCntr").html(score);
-						check();
-					}; 
-					if ($(this).is("#crys3")){
-						console.log(crystals[2]);
-						score += crystals[2];
-						$(".scoreCntr").html(score);
-						check();
-					}; 
-					if ($(this).is("#crys4")){
-						console.log(crystals[3]);
-						score += crystals[3];
-						$(".scoreCntr").html(score);
-						check();
-					}; 
-
-			}); 
-		}
 });
